88aea50cf56334b11cb09757655ff05f6cd758f5
moruo44 <734733711@qq.com>
2018-11-12 15:21:45 +0800

    feat: add soft-bind test

--- softBind/softBind.js
+++ softBind/softBind.js
@@ -1,7 +1,7 @@
 // 多次band就像栈一样，最先绑定的最后执行，所以就会复写完成绑定的this，所以不能更改绑定的对象
 
 // 思路1: 维护最新的想要bind to的target
-Function.prototype.bind = (function generateBind() {
+const bind1 = (function generateBind() {
   let target = null
 
 	return function(obj, ...args) {
@@ -9,16 +9,16 @@ Function.prototype.bind = (function generateBind() {
     const curried = args
     const that = this
     return function(...args){
-      return that.call(
+      return that.apply(
         target,
-        ...[...curried, ...args]
+        [...curried, ...args]
       )
     }
 	}
 })()
 
 // 思路2： 如果已经被绑定了this，就不再绑定新的this
-Function.prototype.bind = function(obj, ...args) {
+function bind2(obj, ...args) {
   const fn = this
   const curried = args
 
@@ -28,9 +28,14 @@ Function.prototype.bind = function(obj, ...args) {
       (typeof global !== "undefined" && this === global)
     ) ? obj : this
 
-    return fn.call(
+    return fn.apply(
       target,
-      ...[...curried, ...args]
+      [...curried, ...args]
     )
   }
+}
+
+module.exports = {
+  bind1,
+  bind2,
 }
